fd84c5fa573820295e44e4bd6d888b04682e2810
kingstar718 <kingstar718@foxmail.com>
2022-11-09 00:33:13 +0800

    return and error

---
+++ src/return_error_demo/mod.rs
@@ -0,0 +1,4 @@
+mod return_error_demo;
+mod test;
+
+pub use return_error_demo::read_username_from_file;
---
+++ src/return_error_demo/test.rs
@@ -0,0 +1,6 @@
+use crate::return_error_demo::read_username_from_file;
+
+#[test]
+pub fn read_username_from_file_test() {
+    read_username_from_file();
+}
